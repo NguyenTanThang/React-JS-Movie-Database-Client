6a4fe3bc8fa0c2ab425b39a011406b49ae5072a6
Victor <nguyentanthang9a4@gmail.com>
2020-11-24 08:47:07 +0700

    Version 1.1.2 - Authenticate before online payment

--- src/pages/MomoPaymentPage.js
+++ src/pages/MomoPaymentPage.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import {getPayURL} from "../requests/momoRequests";
-import {getSubStatus} from "../requests/authRequests";
+import {getSubStatus, getAuthStatus} from "../requests/authRequests";
+import {message} from "antd";
 
 class MomoPaymentPage extends Component {
 
     async componentDidMount() {
         const subStatus = await getSubStatus();
+        const loggedIn = await getAuthStatus();
+        if (!loggedIn) {
+            message.error("You need to login to perform payment")
+            return this.props.history.push("/sign-in");
+        }
         if (subStatus === "active") {
-            this.props.history.push("/");
+            return this.props.history.push("/");
         }
         const customerID = localStorage.getItem("userID")
         const amount = localStorage.getItem("amount")
--- src/pages/StripePayPage.js
+++ src/pages/StripePayPage.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import StripeApp from "../components/stripe/StripeApp";
 import Navbar from "../components/partials/Navbar";
-import {getSubStatus} from "../requests/authRequests";
+import {getSubStatus, getAuthStatus} from "../requests/authRequests";
+import {message} from "antd";
 
 class StripePayPage extends Component {
 
     async componentDidMount() {
         const subStatus = await getSubStatus();
+        const loggedIn = await getAuthStatus();
+        if (!loggedIn) {
+            message.error("You need to login to perform payment")
+            return this.props.history.push("/sign-in");
+        }
         if (subStatus === "active") {
-            this.props.history.push("/");
+            return this.props.history.push("/");
         }
     }
 
