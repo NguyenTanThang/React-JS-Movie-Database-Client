85fb02860d3f20a1362eb58c8dc1ac8212389462
Victor <nguyentanthang9a4@gmail.com>
2020-11-25 09:12:44 +0700

    Version 1.1.3 - Config backend URL variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const MAIN_PROXY_URL = `http://localhost:5000`;
-//export const MAIN_PROXY_URL = `https://node-js-movie-database-backend.herokuapp.com`;
+//export const MAIN_PROXY_URL = `http://localhost:5000`;
+export const MAIN_PROXY_URL = `https://node-js-movie-database-backend.herokuapp.com`;
 export const customerID = `5f700f89bf9e1f32f4e8b617`;
